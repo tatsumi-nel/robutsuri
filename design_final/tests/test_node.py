ead713d8b187a35d9ef8d946670f722b3933b606
tatsumi-nel <37326023+tatsumi-nel@users.noreply.github.com>
2018-05-09 12:00:01 +0900

    固有値計算方法の変更とテスト追加

--- design_final/tests/test_node.py
+++ design_final/tests/test_node.py
@@ -2,6 +2,7 @@ import unittest
 
 import sys
 sys.path.append('../lib')
+import math
 
 from config import *
 from cross_section import CrossSection
@@ -9,10 +10,10 @@ from node import Node
 
 class NodeTest(unittest.TestCase):
 
-    def test_uniform(self):
+    def test_onenode(self):
         node = Node()
         xs = CrossSection([1.36, 0.0181, 0.0279])
-        xs.debug()
+        #xs.debug()
         node.set_xs(xs)
         node.set_keff( xs.nusigf() / xs.siga() )    
         node.calc()
@@ -26,12 +27,10 @@ class NodeTest(unittest.TestCase):
         self.assertEqual(node.get_jout(XM)+node.get_jin(XM), node.get_flux() / 2.0)
 
 
-
-    def test_twonodes(self):
+    def test_uniform_zeroflux_bc(self):
         xs_fuel = CrossSection([1.36, 0.0181, 0.0279])
-        xs_ref  = CrossSection([0.55, 0.0127, 0.0])
         delta = 1.0
-        geom = [{'xs':xs_ref, 'width':30}, {'xs':xs_fuel, 'width':60}, {'xs':xs_ref, 'width':30} ]
+        geom = [{'xs':xs_fuel, 'width':100}]
 
         nodes = []        
         for r in geom:
@@ -41,7 +40,8 @@ class NodeTest(unittest.TestCase):
                 nodes.append(the_node)
         
         keff_old = 1.0
-        conv = 1.0e-5
+        pro_old = 1.0
+        conv = 1.0e-7
         for ik in range(2000):  # outer iteration
 
             for istart in range(2):  # start color (0: red, 1:black)
@@ -66,21 +66,89 @@ class NodeTest(unittest.TestCase):
                 the_node = nodes[ix]
                 rr = rr + the_node.get_xs() * the_node.get_flux() * the_node.get_width()
             
-            keff = rr.nusigf() / rr.siga()
+            #keff = rr.nusigf() / rr.siga()
+            keff = rr.nusigf() / (pro_old/keff_old)
             diff = abs((keff - keff_old)/keff)
-            print( keff, diff)
+            #print( keff, diff)
             if(diff < conv):
                 break
-            keff_old = keff            
+            keff_old = keff
+            pro_old = rr.nusigf()
 
             for ix in range(len(nodes)):
                 nodes[ix].set_keff(keff)
             
         # debug
-        print("flux")
-        for ix in range(len(nodes)):
-            print(ix, nodes[ix].get_flux())
+        #print("flux")
+        #for ix in range(len(nodes)):
+        #    print(ix, nodes[ix].get_flux())
+    
+        
+        kana = xs_fuel.nusigf() / (xs_fuel.dif() * math.pi ** 2 / 100**2 + xs_fuel.siga())
+        #print( 'kana = ', kana)
+        self.assertAlmostEqual(keff, kana, places=5)
 
+
+    def test_two_regions_zeroflux_bc(self):
+        xs_fuel = CrossSection([1.36, 0.0181, 0.0279])
+        xs_ref  = CrossSection([0.55, 0.0127, 0.0])
+        delta = 0.1
+        geom = [{'xs':xs_ref, 'width':60}, {'xs':xs_fuel, 'width':60}, {'xs':xs_ref, 'width':60} ]
+
+        nodes = []        
+        for r in geom:
+            for k in range(int(r['width']/delta)):
+                the_node = Node(r['xs'])
+                the_node.set_width(delta)
+                nodes.append(the_node)
+        
+        keff_old = 1.0
+        pro_old = 1.0
+        conv = 1.0e-10
+        for ik in range(3000):  # outer iteration
+
+            for istart in range(2):  # start color (0: red, 1:black)
+                for ix in range(istart, len(nodes), 2):
+                    if(ix==0):
+                        jin_xm = -nodes[ix].get_jout(XM)
+                    else:
+                        jin_xm = nodes[ix-1].get_jout(XP)
+                    
+                    if(ix==len(nodes)-1):
+                        jin_xp = -nodes[ix].get_jout(XP)
+                    else:
+                        jin_xp = nodes[ix+1].get_jout(XM)
+
+                    nodes[ix].set_jin(XM, jin_xm)
+                    nodes[ix].set_jin(XP, jin_xp)
+                    nodes[ix].calc()
+
+            # red/black done
+            rr = CrossSection()
+            for ix in range(len(nodes)):
+                the_node = nodes[ix]
+                rr = rr + the_node.get_xs() * the_node.get_flux() * the_node.get_width()
+            
+            #keff = rr.nusigf() / rr.siga()
+            keff = rr.nusigf() / (pro_old/keff_old)
+            diff = abs((keff - keff_old)/keff)
+            if( ik % 100 == 0):
+                print( ik, keff, diff)
+            if(diff < conv):
+                break
+            keff_old = keff
+            pro_old = rr.nusigf()
+
+            for ix in range(len(nodes)):
+                nodes[ix].set_keff(keff)
+            
+        # debug
+        #print("flux")
+        #for ix in range(len(nodes)):
+        #    print(ix, nodes[ix].get_flux())
+
+        print("keff=",keff)
+        self.assertAlmostEqual(keff, 1.41102, places=5)
 
 if __name__ == '__main__':
     unittest.main()
